5e4feb0f5affd163f9907f130ddc8f0ab324eb22
Ayeshmantha Madara Wijegunathileke <ayesh99747@gmail.com>
2023-04-26 11:46:05 +0100

    Completed (Task4) Compute Jaccard similarity

--- src/main/java/example/project/CodeClones/JaccardSimilarity.java
+++ src/main/java/example/project/CodeClones/JaccardSimilarity.java
@@ -6,8 +6,20 @@ import java.util.Set;
 public class JaccardSimilarity {
 
     public static double computeJaccardSimilarity(Set<String> set1, Set<String> set2) {
-        // TODO: implement this function to compute the Jaccard similarity between two string arrays
-        return -1.0;
+        int intersectionSize = 0; // A ∩ B
+        int unionSize = set1.size() + set2.size(); // A ∪ B
+        for (String str : set1) {
+            // Iterating through each string in the set to see if there are similar strings.
+            if (set2.contains(str)) {
+                // If the string is present in both sets.
+                intersectionSize++;
+                unionSize--;
+            }
+        }
+        /*
+            Jaccard Similarity = (A ∩ B) / (A ∪ B)
+         */
+        return (double) intersectionSize / unionSize;
     }
 
 }
