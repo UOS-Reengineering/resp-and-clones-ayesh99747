729a49d991d09e4e3c4937819b13e1c2407defd7
Ayeshmantha Madara Wijegunathileke <ayesh99747@gmail.com>
2023-04-26 11:57:23 +0100

    Completed (Task5) Draw dot plots

--- src/main/java/example/project/CodeClones/DotPlot.java
+++ src/main/java/example/project/CodeClones/DotPlot.java
@@ -3,7 +3,26 @@ package example.project.CodeClones;
 public class DotPlot {
 
     public static void printDotPlot(char[] sequence1, char[] sequence2) {
-        // TODO: complete this function to print the dot plot of two string arrays
+
+        // Printing out the first line of characters in sequence2.
+        System.out.print("\t");
+        for (int j = 0; j < sequence2.length; j++) {
+            System.out.print(sequence2[j] + "\t");
+        }
+        System.out.println();
+
+        // Iterate through both sequences.
+        for (int i = 0; i < sequence1.length; i++) {
+            System.out.print(sequence1[i] + "\t"); // Printing out the first sequence in each row,
+            for (int j = 0; j < sequence2.length; j++) {
+                if (sequence1[i] == sequence2[j]) {
+                    System.out.print("+\t"); // If there is a match print out a +.
+                } else {
+                    System.out.print(" \t"); // If there is no match print out a ' '.
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
